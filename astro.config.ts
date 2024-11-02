6d913aadc78b8a6aa3cd874aadd14711e7806855
ex3ploiter <masoudhadi642924@gmail.com>
2024-11-02 19:22:29 +0330

    Fix Vis

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,17 @@
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 import { defineConfig } from 'astro/config';
+import type { Plugin as VitePlugin } from 'vite';
 import { visualizer } from 'rollup-plugin-visualizer';
+
 
 export default defineConfig({
   site: 'https://ex3ploiter.github.io',
   integrations: [tailwind(), compress()],
   vite: {
-    plugins: [visualizer()],
+    plugins: [
+      visualizer() as VitePlugin
+    ],
     build: {
       target: 'esnext',
     },
