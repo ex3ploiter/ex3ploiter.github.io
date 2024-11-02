9b93732f7f39869d243cc9cb589c130f4b376812
ex3ploiter <masoudhadi642924@gmail.com>
2024-11-02 19:28:03 +0330

    prt

--- astro.config.ts
+++ astro.config.ts
@@ -4,14 +4,11 @@ import { defineConfig } from 'astro/config';
 import type { Plugin as VitePlugin } from 'vite';
 import { visualizer } from 'rollup-plugin-visualizer';
 
-
 export default defineConfig({
   site: 'https://ex3ploiter.github.io',
   integrations: [tailwind(), compress()],
   vite: {
-    plugins: [
-      visualizer() as VitePlugin
-    ],
+    plugins: [visualizer() as VitePlugin],
     build: {
       target: 'esnext',
     },
