79ee6611fb80b5af82d7764e7ee9036874f9ae4f
ex3ploiter <masoudhadi642924@gmail.com>
2024-11-02 16:42:42 +0330

    sharp

--- astro.config.ts
+++ astro.config.ts
@@ -3,14 +3,21 @@ import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 import { defineConfig } from 'astro/config';
 import { visualizer } from 'rollup-plugin-visualizer';
+import wasm from 'vite-plugin-wasm';
+
 
 // https://astro.build/config
 export default defineConfig({
   // output: 'server',
   site:'https://ex3ploiter.github.io',
-  integrations: [tailwind(), image(), compress()],
+  integrations: [tailwind(),
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp',
+    }),
+    compress(),
+  ],
   vite: {
-    plugins: [visualizer()],
+    plugins: [visualizer(), wasm()],
     resolve: {
       alias: {
         // 'date-fns/locale': 'date-fns/locale/index.js',
